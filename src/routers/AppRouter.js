f1d6bfb775f54f6f78388cfed6b3e1667c34cef6
GM-WebdevelopmentDuesseldorf <WebDevelopmentDuesseldorf@gmail.com>
2021-07-09 05:40:25 +0200

    fixing boilerplate

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const AppRouter = (
-    <BrowserRouter>
+   
         <div>
             <h1>Empty Router</h1>
         </div>
-    </BrowserRouter>
+   
 );
 
 export default AppRouter;
