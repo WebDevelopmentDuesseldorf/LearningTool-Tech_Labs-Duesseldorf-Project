ad55cc7acbdfc5b04889d0022d8cce8070d05802
GM-WebdevelopmentDuesseldorf <WebDevelopmentDuesseldorf@gmail.com>
2021-07-17 06:03:44 +0200

    Redux-React 7.2.4 installed. HOC Data TaskList added, Provider can get data from Store, TaskList is Connectet to mapStateToProps,  Store gets displayed on scheduler dashboard, New Files in /components TaskList.js TaskListItem.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css';
@@ -11,30 +12,19 @@ import  getVisibleTasks  from './selectors/tasks';
 
 const store = configureStore();
 
-
-const task1 = store.dispatch(addTask({ description: 'Water bill', timeframe: 200 }));
-
-const task2 = store.dispatch(addTask({ description: 'Gas bill', timeframe: 300 }));
+const task1 = store.dispatch(addTask({ description: 'Gitarre lernen', timeframe: 22000 }));
+const task2 = store.dispatch(addTask({ description: 'Learn Chinese', timeframe: 12300 }));
 
 // setTextFilter -> bill (2 items) -> water (1 item)
-store.dispatch(setTextFilter('bill'))
+store.dispatch(setTextFilter('Chi'))
 // store.dispatch(setTextFilter('water'))
-
+setTimeout(() => store.dispatch(setTextFilter('i')), 5000)
 // print visible ones to screen
-const state = store.getState()
-const visibleTasks = getVisibleTasks(state.tasks, state.filters);
-const Tasks = () => (
-    <div>
-        {JSON.stringify(visibleTasks)}
 
-    </div>
-
+const jsx = (
+    <Provider store={store}>
+        <AppRouter /> 
+    </Provider>
 )
 
-
-console.log(visibleTasks)
-
-
-
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
