7c7580d4e32e13b8af2b9cc7560eb2d44fd109ab
TimoKonein <timo-konein@web.de>
2021-08-01 19:42:19 +0200

    installedServerOnline Computer

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => (
     <div>
-        <h1>Welcome to LearningTool</h1>
+        <h1>Welcome to </h1>
         <p>This page is under construction</p>
     </div>
 )
