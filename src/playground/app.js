59700caeb6c075ba496817220c030a62a451e50f
Marius <westers.marius@gmail.com>
2021-08-01 22:50:49 +0200

    Crashkurs React Auth Firebase Min 24:15

--- src/playground/app.js
+++ src/playground/app.js
@@ -7,11 +7,14 @@ import { Container } from "react-bootstrap"
 
 function App() {
     return (
-        <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh"}} >
-        <div className="w-100" style={{maxWidth: "400px"}}>
-            <Signup />  
-        </div>
-        </Container>
+        <AuthProvider>
+            <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh"}} >
+                <div className="w-100" style={{maxWidth: "400px"}}>
+                <Signup />  
+                </div>
+            </Container>
+        </AuthProvider>
+        
     ) 
 }
 
