e107a0058b2dd1c68797b74e74152bd029acd354
Marius <westers.marius@gmail.com>
2021-08-01 21:53:39 +0200

    Crashkurs React Auth Firebase Min 15:50

--- src/playground/components/Signup.js
+++ src/playground/components/Signup.js
@@ -1,8 +1,11 @@
-import React from "react"
-
+import React, { useRef, useState } from "react"
 import { Form, Button, Card } from "react-bootstrap"
 
 export default function Signup(){
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+
     return (
     <> 
         <Card>
@@ -11,9 +14,19 @@ export default function Signup(){
                 <Form>
                     <Form.Group id="email">
                         <Form.Label>Email</Form.Label>
-                    
-
+                        <Form.Control type="email" ref={emailRef} required />
                     </Form.Group>
+                    <Form.Group id="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" ref={passwordRef} required />
+                    </Form.Group>
+                    <Form.Group id="password-confirm">
+                        <Form.Label>Password Confirmation</Form.Label>
+                        <Form.Control type="password" ref={passwordConfirmRef} required />
+                    </Form.Group>
+                    <Button className="w-100" type="submit">
+                    Sign Up
+                    </Button>
                 </Form>
             </Card.Body>
         </Card>
