f4740fa2ad780aa2bb3c2587f745b9c1344987e5
GM-WebdevelopmentDuesseldorf <WebDevelopmentDuesseldorf@gmail.com>
2021-08-05 01:04:34 +0200

    Login works now, new componentFile Login.js - React Authentication Crash Course With Firebase And Routing Min34 - Gian

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-import React from "react"
-import Signup from "./Signup";
+import React from "react";
+import SignUp from "./SignUp";
+import LogIn from "./LogIn";
+import Dashboard from "./Dashboard"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Dashboard from "./Dashboard"
 
-function App() {
-    console.log(process.env.REACT_APP_FIREBASE_API_KEY)
+
+
+
+function App(){
+    
     return (
             <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh"}} >
                 <div className="w-100" style={{maxWidth: "400px"}}>
@@ -15,7 +19,8 @@ function App() {
                     <AuthProvider>
                         <Switch>
                             <Route exact path="/" component={Dashboard} />
-                            <Route path="/signup" component={Signup} />
+                            <Route path="/signup" component={SignUp} />
+                            <Route path="/login" component={LogIn} />
                         </Switch>
                     </AuthProvider>
                 </Router>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,14 +1,16 @@
 import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
 
-export default function Signup(){
+export default function SignUp(){
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
     const { signup } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
+    const history = useHistory()
 
    async function handleSubmit(e) {
         e.preventDefault()
@@ -24,6 +26,7 @@ export default function Signup(){
             setError('')
             setLoading(true)
             await signup(emailRef.current.value, passwordRef.current.value)
+            history.push('/')
         }   catch {
             setError('Failed to create an account')
         }
@@ -56,7 +59,7 @@ export default function Signup(){
             </Card.Body>
         </Card>
         <div className="w-100 text-center mt-2">
-            Already have an account? Log In
+            Already have an account? <Link to="/login">Log In</Link>
         </div>
         </>
     )
