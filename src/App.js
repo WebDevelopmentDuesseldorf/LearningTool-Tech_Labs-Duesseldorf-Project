86a5199065dbbdc7f5f895ea51b88f7bea86ad7e
GM-WebdevelopmentDuesseldorf <WebDevelopmentDuesseldorf@gmail.com>
2021-09-02 04:37:23 +0200

    started with empty folder and created react app from scratch, added env.local

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './config/theme.config';
-
-import Dashboard from './pages/Dashboard';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <Router>
-      <ThemeProvider theme={theme}>
-        <Switch>
-          <Route exact path="/">
-            <SignIn />
-          </Route>
-          <Route path="/sign-up">
-            <SignUp />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </ThemeProvider>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
